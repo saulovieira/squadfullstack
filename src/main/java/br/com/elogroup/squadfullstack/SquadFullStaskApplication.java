01485dbba8374ea5eb4980d0109d206d32756c0b
Saulo Vieira <saulo.vieira@elogroup.com.br>
2023-02-23 10:20:47 -0300

    Ajuste no nome da Classe de execução do backend

--- src/main/java/br/com/elogroup/squadfullstack/SquadFullStaskApplication.java
+++ src/main/java/br/com/elogroup/squadfullstack/SquadFullStaskApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SqaudfullstaskApplication {
+public class SquadFullStaskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SqaudfullstaskApplication.class, args);
+		SpringApplication.run(SquadFullStaskApplication.class, args);
 	}
 
 
